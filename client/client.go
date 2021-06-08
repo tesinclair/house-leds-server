212b9481a36b68b2356482dcf8636cbf899c4240
tesinclair <66572748+tesinclair@users.noreply.github.com>
2021-06-08 17:05:17 +0100

    initial (?) commit for house leds server

--- client/client.go
+++ client/client.go
@@ -3,7 +3,7 @@ package client
 import (
 	"net"
 	"log"
-	"github.com/nu7hatch/gouuid"
+	//"github.com/nu7hatch/gouuid"
 )
 
 type Client struct {
@@ -13,12 +13,12 @@ type Client struct {
 }
 
 func NewClient(c net.Conn, reciever bool) *Client{
-	u, err := uuid.NewV4()
-	if err != nil{
-		log.Fatal("unable to assign uuid to client")
-	}
+	//u, err := uuid.NewV4()
+	// if err != nil{
+	// 	log.Fatal("unable to assign uuid to client")
+	// }
 	return &Client{
-		Id: u.String(),
+		Id: "something-here-uniquey",
 		Connection: c,
 		isReciever: reciever,
 	}
